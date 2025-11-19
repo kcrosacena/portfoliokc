b139e795feccb9f907f4d8c42c38dc1bd000c5d2
Sam <samananiascases@gmail.com>
2025-11-19 09:54:14 +0800

    directory fixing

--- main/header.php
+++ main/header.php
@@ -13,11 +13,11 @@
 
     <nav>
         <ul>
-            <li><a href="home.php" class="nav-link">Home</a></li>
-            <li><a href="about.php" class="nav-link">About</a></li>
-            <li><a href="personalinfo.php" class="nav-link">Personal Info</a></li>
-            <li><a href="education.php" class="nav-link">Education</a></li>
-            <li><a href="contact.php" class="nav-link">Contact</a></li>
+            <li><a href="/main/home.php" class="nav-link">Home</a></li>
+            <li><a href="/main/about.php" class="nav-link">About</a></li>
+            <li><a href="/main/personalinfo.php" class="nav-link">Personal Info</a></li>
+            <li><a href="/main/education.php" class="nav-link">Education</a></li>
+            <li><a href="/main/contact.php" class="nav-link">Contact</a></li>
             <li><a href="../projects/create_project.php" class="nav-link">Projects</a></li>
         </ul>
     </nav>
